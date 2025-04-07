b7baa56b74c8540b4816c92477de49a1f3bd59e7
OLiUniville <olivandro.sabbi@univille.br>
2025-04-07 19:19:40 -0300

    Estrutura

---
+++ arithmosotto/arithmosotto/src/main/java/com/arithmosotto/arithmosotto/ArithmosottoApplication.java
@@ -0,0 +1,13 @@
+package com.arithmosotto.arithmosotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmosottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmosottoApplication.class, args);
+	}
+
+}
---
+++ arithmosotto/arithmosotto/src/test/java/com/arithmosotto/arithmosotto/ArithmosottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arithmosotto.arithmosotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmosottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
